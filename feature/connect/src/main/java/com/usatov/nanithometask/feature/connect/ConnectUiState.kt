9b790255565b2ed1b90e62c78f4153f97ef40b96
Oleksandr Usatov <oleksandr.usatov@gmail.com>
2025-06-25 23:33:34 +0200

    removed unnecasary placeholders

--- feature/connect/src/main/java/com/usatov/nanithometask/feature/connect/ConnectUiState.kt
+++ feature/connect/src/main/java/com/usatov/nanithometask/feature/connect/ConnectUiState.kt
@@ -1,8 +1,8 @@
 package com.usatov.nanithometask.feature.connect
 
 data class ConnectUiState(
-    val ip: String = "192.168.0.147",
-    val port: String = "8080",
+    val ip: String = "",
+    val port: String = "",
     val status: Status = Status.Idle,
     val errorMessage: String? = null
 ) {
